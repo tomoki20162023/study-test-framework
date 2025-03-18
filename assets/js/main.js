ce58a56422e21a509294ff546e650caac008278d
tmk.ggl81 <tmk.ggl81@gmail.com>
2025-03-19 00:01:11 +0900

    todo除外およびasset追加

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
-$(function() => {
+$(() => {
 	console.debug("start.");
+	$('#test').on('click', t);
 });
+
+function t() {
+	console.debug("begin t().");
+	console.debug("ends  t().");
+}
 
 console.debug("loaded.");
